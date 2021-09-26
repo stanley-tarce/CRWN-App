d0892409519b792bca2560087218f6007fe5ebad
Stanley Tarce <stanleytarce18@gmail.com>
2021-09-26 19:14:47 +0800

    Created React app and Main Page

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Homepage from './Components/Homepage/Homepage';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
